32ae7b8bfecc7ff4051556d52c4e98ae4fec50c8
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-03-27 09:59:13 +0545

    some basic opetation

--- test.dart
+++ test.dart
@@ -1,6 +1,12 @@
 void main() {
   int a = 10;
   int b = 20;
-  int c = a + b;
-  print(c);
+  // int c = a + b;
+  // print(c);
+
+  if (a > b) {
+    print("a is greater than b");
+  } else {
+    print("b is greater than a");
+  }
 }
